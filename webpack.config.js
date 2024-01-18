519d8f9fb1caec9e2a4be74e3651e44fe7421481
luan.correia <luan.correia@intelie.com.br>
2024-01-18 18:30:12 -0300

    format file

--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,32 @@
-import path from 'path'
-import {execSync as exec} from 'child_process'
-import HtmlWebpackPlugin  from 'html-webpack-plugin'
+import path from "path";
+import { execSync as exec } from "child_process";
+import HtmlWebpackPlugin from "html-webpack-plugin";
 
-const appsNames = exec(`ls ./apps/`, {encoding: 'utf-8'}).toString()
-const mainApp = {index: path.resolve('src', 'index.js')}
+const appsNames = exec(`ls ./apps/`, { encoding: "utf-8" }).toString();
+const mainApp = { index: path.resolve("src", "index.js") };
 
+const isDevelopmentMode = process.env.BUILDMODE == "DEV";
 
-const entryPoints = appsNames.split('\n').reduce((acc, currentAppName) => {
-    if(!currentAppName) return acc
+const entryPoints = appsNames.split("\n").reduce((acc, currentAppName) => {
+  if (!currentAppName) return acc;
 
-    const pathApp = {[currentAppName]: path.resolve('apps', currentAppName, 'index.js')}
-    return {...acc,  ...pathApp}
-
-},mainApp)
-
+  const pathApp = {
+    [currentAppName]: path.resolve("apps", currentAppName, "index.js"),
+  };
+  return { ...acc, ...pathApp };
+}, mainApp);
 
 export default {
   entry: entryPoints,
-  devtool: 'source-map',
+  devtool: isDevelopmentMode ? "source-map" : "eval",
   output: {
-    filename: '[name].bundle.js',
-    path:path.resolve('dist'),
-    clean: true
+    filename: "[name].bundle.js",
+    path: path.resolve("dist"),
+    clean: true,
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: 'src/index.html'
-    })
+      template: "src/index.html",
+    }),
   ],
 };
