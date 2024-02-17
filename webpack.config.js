3b810685f0c22984bffba21242340a749571df8a
luan.correia <luan.correia@intelie.com.br>
2024-02-17 10:57:41 -0300

    remove clean on dist directory before build

--- webpack.config.js
+++ webpack.config.js
@@ -24,7 +24,6 @@ module.exports = {
   output: {
     filename: "[name].bundle.js",
     path: path.resolve(__dirname, "dist"),
-    clean: true,
   },
   devServer: {
     historyApiFallback: true,
